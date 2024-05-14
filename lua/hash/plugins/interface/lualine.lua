9464729912935a28d7ec554b0dd3c2e825dcfbc6
Hashino <iam.ravara@gmail.com>
2024-05-14 16:51:01 -0300

    removed unsued variable

--- lua/hash/plugins/interface/lualine.lua
+++ lua/hash/plugins/interface/lualine.lua
@@ -1,6 +1,4 @@
 local clients_lsp = function()
-  local bufnr = vim.api.nvim_get_current_buf()
-
   local clients = vim.lsp.get_active_clients { bufnr = 0 }
   if next(clients) == nil then
     return ''
