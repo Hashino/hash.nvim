05869bac496ec1aee9f0f69d146995fc2b14d84d
Hashino <iam.ravara@gmail.com>
2024-05-14 02:58:22 -0300

    added ascii art

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+-- by Hashino https://github.com/Hashino/hash.nvim/
+ -- _               _                  _           
+ -- | |             | |                (_)          
+ -- | |__   __ _ ___| |__    _ ____   ___ _ __ ___  
+ -- | '_ \ / _` / __| '_ \  | '_ \ \ / / | '_ ` _ \ 
+ -- | | | | (_| \__ \ | | |_| | | \ V /| | | | | | |
+ -- |_| |_|\__,_|___/_| |_(_)_| |_|\_/ |_|_| |_| |_|
+ --
 require 'hash.opts' -- neovim options
 require 'hash.keymaps' -- global keymaps.
 -- NOTE: specific plugin keymaps defined inside plugin config
