25b05366e4f43b9fb579fc2f72edc3851bbe18e5
Hashino <iam.ravara@gmail.com>
2024-05-14 02:05:05 -0300

    tweaked lsp configo (lua globals)

--- lua/hash/plugins/formatting/lsp-config.lua
+++ lua/hash/plugins/formatting/lsp-config.lua
@@ -28,7 +28,7 @@ return { -- LSP Configuration & Plugins
               callSnippet = 'Replace',
             },
             -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
+            diagnostics = { globals = { 'vim', 'screen', 'awesome', 'root', 'client', 'tag' } },
           },
         },
       },
