870e6329c733ea9d6dfe9a4b5ff646c8d150ee28
Hashino <iam.ravara@gmail.com>
2024-05-17 17:29:29 -0300

    added default values

--- lua/hash/theme.lua
+++ lua/hash/theme.lua
@@ -2,6 +2,30 @@ return { -- Theme
   'rmehri01/onenord.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
+    require('onenord').setup {
+      custom_colors = {
+        fg = '#C8D0E0',
+        fg_light = '#E5E9F0',
+        bg = '#2E3440',
+        gray = '#646A76',
+        light_gray = '#6C7A96',
+        cyan = '#88C0D0',
+        blue = '#81A1C1',
+        dark_blue = '#5E81AC',
+        green = '#A3BE8C',
+        light_green = '#8FBCBB',
+        dark_red = '#BF616A',
+        red = '#D57780',
+        light_red = '#DE878F',
+        pink = '#E85B7A',
+        dark_pink = '#E44675',
+        orange = '#D08F70',
+        yellow = '#EBCB8B',
+        purple = '#B988B0',
+        light_purple = '#B48EAD',
+        none = 'NONE',
+      },
+    }
     vim.cmd.colorscheme 'onenord' -- Load the colorscheme here.
   end,
 }
