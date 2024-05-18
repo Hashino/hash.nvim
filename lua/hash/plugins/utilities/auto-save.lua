0fdc2f91211fdce7e69e0a4b292c71e0475cdfd6
Hashino <iam.ravara@gmail.com>
2024-05-18 15:37:15 -0300

    fix(auto-save): no longes auto saves git commits msgs

--- lua/hash/plugins/utilities/auto-save.lua
+++ lua/hash/plugins/utilities/auto-save.lua
@@ -1,10 +1,23 @@
-return {  -- Auto-saves on exit insert mode
+return { -- Auto-saves on exit insert mode
   'Pocco81/auto-save.nvim',
   config = function()
     require('auto-save').setup {
       -- your config goes here
       -- or just leave it empty :)
       trigger_events = { 'InsertLeave' },
+      condition = function(buf)
+        local fn = vim.fn
+        local utils = require 'auto-save.utils.data'
+
+        if
+          fn.getbufvar(buf, '&modifiable') == 1
+          and utils.not_in(fn.getbufvar(buf, '&filetype'), {})
+          and not string.find(fn.expand '%', '.git/COMMIT_EDITMSG') -- ignore git commit messages
+        then
+          return true -- met condition(s), can save
+        end
+        return false -- can't save
+      end,
     }
   end,
 }
