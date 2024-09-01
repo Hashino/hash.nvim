844b395a50a024e40fd87d34b612d02eed46adc1
Hashino <iam.ravara@gmail.com>
2024-08-31 22:08:11 -0300

    changed breadcrumbs plugin
    now its barbecue

--- lua/hash/plugins/interface/breadcrumbs.lua
+++ lua/hash/plugins/interface/breadcrumbs.lua
@@ -1,14 +1,22 @@
 return {
-  'Bekaboo/dropbar.nvim',
+  "utilyre/barbecue.nvim",
+  name = "barbecue",
+  event = "LspAttach",
+  dependencies = { "SmiteshP/nvim-navic", "nvim-tree/nvim-web-devicons" },
   config = function()
-    require 'dropbar'.setup {
-      sources = {
-        path = {
-          relative_to = function()
-            return vim.api.nvim_buf_get_name(0)
-          end
-        }
-      }
+    vim.opt.updatetime = 200
+    require("barbecue").setup {
+      create_autocmd = false,
+      attach_navic = true,
+      show_dirname = false,
+      show_basename = false,
     }
-  end
+
+    vim.api.nvim_create_autocmd({ "BufWinEnter", "CursorHold", "BufModifiedSet" }, {
+      group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+      callback = function()
+        require("barbecue.ui").update()
+      end,
+    })
+  end,
 }
