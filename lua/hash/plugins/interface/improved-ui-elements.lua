0c10add8e5bd538ceee858edd1b45e233ae9a941
Hashino <iam.ravara@gmail.com>
2024-09-01 23:04:57 -0300

    feat(ui-elements): floating notifications
    installed rcarriga/vim-notify
    
    since I've switched to a floating command window, I've hidden the
    default cmd line which made notifications invisible. should fix this

--- lua/hash/plugins/interface/improved-ui-elements.lua
+++ lua/hash/plugins/interface/improved-ui-elements.lua
@@ -1,4 +1,16 @@
-return { -- improve ui elements (float boxes)
-  'stevearc/dressing.nvim',
-  opts = {},
+return {
+  { -- improve ui elements (float boxes)
+    'stevearc/dressing.nvim',
+    opts = {},
+  },
+  {
+    'rcarriga/nvim-notify',
+    config = function()
+      vim.notify = require('notify')
+      require 'notify'.setup {
+        render = 'wrapped-compact',
+        stages = 'static'
+      }
+    end
+  }
 }
