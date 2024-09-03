d2622fe57db7fba46e3f8067116f28485becb622
Hashino <iam.ravara@gmail.com>
2024-09-03 02:04:08 -0300

    small cleanups

--- lua/hash/plugins/utilities/auto-save.lua
+++ lua/hash/plugins/utilities/auto-save.lua
@@ -4,25 +4,20 @@ return { -- Auto-saves on exit insert mode
   config = function()
     local fidget = require 'fidget'
 
-    fidget.notification.set_config('autosave',
-      { name = '', icon = '' }, false)
+    fidget.notification.set_config('autosave', { name = '', icon = '' }, false)
 
     require('auto-save').setup {
       -- your config goes here
       -- or just leave it empty :)
       trigger_events = { 'InsertLeave' },
       condition = function(buf)
-        local fn = vim.fn
         local utils = require 'auto-save.utils.data'
 
-        if
-           fn.getbufvar(buf, '&modifiable') == 1
-           and utils.not_in(fn.getbufvar(buf, '&filetype'), {})
-           and not string.find(fn.expand '%', '.git/COMMIT_EDITMSG') -- ignore git commit messages
-        then
-          return true                                                -- met condition(s), can save
-        end
-        return false                                                 -- can't save
+        return
+           vim.fn.getbufvar(buf, '&modifiable') == 1
+           and utils.not_in(vim.fn.getbufvar(buf, '&filetype'), {})
+           -- ignore git commit messages
+           and not string.find(vim.fn.expand '%', '.git/COMMIT_EDITMSG')
       end,
       execution_message = {
         message = function() -- message to print on save
@@ -31,8 +26,8 @@ return { -- Auto-saves on exit insert mode
 
           return ''
         end,
-        dim = 0.18,               -- dim the color of `message`
-        cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+        dim = 0.18,
+        cleaning_interval = 1250,
       },
     }
   end,
