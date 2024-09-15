0eddd18d7b83919a199df056e6e7c82152ff37e7
Hashino <iam.ravara@gmail.com>
2024-09-15 19:39:07 -0300

    fix: nvim warning for termguicolors

--- lua/hash/opts.lua
+++ lua/hash/opts.lua
@@ -10,7 +10,7 @@ vim.g.maplocalleader = " "
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
-vim.g.termguicolors = true
+vim.opt.termguicolors = true
 
 -- Make line numbers default
 vim.opt.number = true
