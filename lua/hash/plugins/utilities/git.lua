ac6739b69028af5a723f3a27e35865ce3960acfa
Hashino <iam.ravara@gmail.com>
2024-10-06 00:23:13 -0300

    experimental: trying diffview

--- lua/hash/plugins/utilities/git.lua
+++ lua/hash/plugins/utilities/git.lua
@@ -33,6 +33,56 @@ return {
     end,
   },
   {
+    "sindrets/diffview.nvim",
+    init = function()
+      local actions = require("diffview.actions")
+      require("diffview").setup({
+        keymaps = {
+          disable_defaults = false, -- Disable the default keymaps
+          view = {
+            { "n", "<leader>gco", actions.conflict_choose("ours"),
+              { desc = "Choose the OURS version of a conflict", }, },
+            { "n", "<leader>gct", actions.conflict_choose("theirs"),
+              { desc = "Choose the THEIRS version of a conflict", }, },
+            { "n", "<leader>gcb", actions.conflict_choose("base"),
+              { desc = "Choose the BASE version of a conflict", }, },
+            { "n", "<leader>gca", actions.conflict_choose("all"),
+              { desc = "Choose all the versions of a conflict", }, },
+            { "n", "<leader>gcO", actions.conflict_choose_all("ours"),
+              { desc = "Choose the OURS version of a conflict for the whole file", }, },
+            { "n", "<leader>gcT", actions.conflict_choose_all("theirs"),
+              { desc = "Choose the THEIRS version of a conflict for the whole file", }, },
+            { "n", "<leader>gcB", actions.conflict_choose_all("base"),
+              { desc = "Choose the BASE version of a conflict for the whole file", }, },
+            { "n", "<leader>gcA", actions.conflict_choose_all("all"),
+              { desc = "Choose all the versions of a conflict for the whole file", }, },
+            { "n", "dX", actions.conflict_choose_all("none"),
+              { desc = "Delete the conflict region for the whole file", }, },
+          },
+          file_panel = {
+            { "n", "?", actions.help("file_panel"),
+              { desc = "Open the help panel", }, },
+            { "n", "<leader>gcO", actions.conflict_choose_all("ours"),
+              { desc = "Choose the OURS version of a conflict for the whole file", }, },
+            { "n", "<leader>gcT", actions.conflict_choose_all("theirs"),
+              { desc = "Choose the THEIRS version of a conflict for the whole file", }, },
+            { "n", "<leader>gcB", actions.conflict_choose_all("base"),
+              { desc = "Choose the BASE version of a conflict for the whole file", }, },
+            { "n", "<leader>gcA", actions.conflict_choose_all("all"), { desc = "Choose all the versions of a conflict for the whole file", }, },
+          },
+          file_history_panel = {
+            { "n", "?", actions.help("file_history_panel"),
+              { desc = "Open the help panel", }, },
+          },
+          option_panel = {
+            { "n", "?", actions.help("option_panel"),
+              { desc = "Open the help panel", }, },
+          },
+        },
+      })
+    end,
+  },
+  {
     "tpope/vim-fugitive",
     config = function()
       vim.keymap.set("n", "<leader>gs", require("telescope.builtin").git_status,
