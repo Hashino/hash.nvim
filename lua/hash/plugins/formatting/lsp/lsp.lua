9cfd8800418eb12bebc8842f701b46c55deaef39
Hashino <iam.ravara@gmail.com>
2024-12-06 16:12:27 -0300

    cleanup

--- lua/hash/plugins/formatting/lsp/lsp.lua
+++ lua/hash/plugins/formatting/lsp/lsp.lua
@@ -76,13 +76,19 @@ return { -- LSP Configuration & Plugins
                 require("barbecue.ui").update()
               end
 
-              -- You will likely want to reduce updatetime which affects CursorHold
-              -- note: this setting is global and should be set only once
-              vim.o.updatetime = 250
-              vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI", }, {
-                group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true, }),
+              -- show diagnostics in floating window
+              vim.api.nvim_create_autocmd("CursorHold", {
+                buffer = bufnr,
                 callback = function()
-                  vim.diagnostic.open_float(nil, { focus = false, })
+                  local opts = {
+                    focusable = false,
+                    close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost", },
+                    border = "rounded",
+                    source = "always",
+                    prefix = " ",
+                    scope = "cursor",
+                  }
+                  vim.diagnostic.open_float(nil, opts)
                 end,
               })
             end,
