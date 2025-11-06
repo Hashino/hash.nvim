182130e74675390d06087db1df7726369a3b02a9
Hashino <iam.ravara@gmail.com>
2025-11-06 15:16:54 -0300

    experimenting with snacks

--- lua/hash/plugins/editing/misc.lua
+++ lua/hash/plugins/editing/misc.lua
@@ -1,16 +1,44 @@
 return {
-  { -- indent lines for context
-    "nvimdev/indentmini.nvim",
-
+  -- { -- indent lines for context
+  --   "nvimdev/indentmini.nvim",
+  --
+  --   config = function()
+  --     require("indentmini").setup({
+  --       char = "┆",
+  --       exclude = { "yaml", },
+  --     })
+  --     local colors = require("hash.theme").colors
+  --
+  --     vim.api.nvim_set_hl(0, "IndentLineCurrent", { fg = colors.light_gray, })
+  --     vim.api.nvim_set_hl(0, "IndentLine", { fg = colors.selection, })
+  --   end,
+  -- },
+  {
+    "folke/snacks.nvim",
+    dependencies = {
+      "rmehri01/onenord.nvim",
+    },
+    event = "InsertEnter",
     config = function()
-      require("indentmini").setup({
-        char = "┆",
-        exclude = { "yaml", },
+      require("snacks").setup({
+        indent = {
+          enable = true,
+          indent = {
+            char = "┆",
+          },
+          scope = {
+            char = "┆",
+          },
+          animate = {
+            enable = false,
+          },
+        },
+
       })
       local colors = require("hash.theme").colors
 
-      vim.api.nvim_set_hl(0, "IndentLineCurrent", { fg = colors.light_gray, })
-      vim.api.nvim_set_hl(0, "IndentLine", { fg = colors.selection, })
+      vim.api.nvim_set_hl(0, "SnacksIndentScope", { fg = colors.light_gray, })
+      vim.api.nvim_set_hl(0, "SnacksIndent", { fg = colors.selection, })
     end,
   },
 
