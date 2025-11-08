50dd014c491220c04d1fd424094339a7b10acced
Hashino <iam.ravara@gmail.com>
2025-11-08 09:37:08 -0300

    fixed colorizer

--- lua/hash/plugins/editing/misc.lua
+++ lua/hash/plugins/editing/misc.lua
@@ -3,9 +3,9 @@ vim.pack.add({
   "https://github.com/VidocqH/auto-indent.nvim",
   "https://github.com/folke/todo-comments.nvim",
   "https://github.com/numToStr/Comment.nvim",
-  -- "https://github.com/norcalli/nvim-colorizer.lua",
   "https://github.com/windwp/nvim-autopairs",
   "https://github.com/folke/trouble.nvim",
+  "https://github.com/norcalli/nvim-colorizer.lua",
 }, { confirm = false, })
 
 require("indentmini").setup({
@@ -49,6 +49,5 @@ end, { desc = "[T]rouble: [S]ymbols", })
 require("auto-indent").setup({})
 require("todo-comments").setup({})
 require("Comment").setup({})
--- WARN: deprecated call
--- require("colorizer").setup({})
 require("nvim-autopairs").setup({})
+require("colorizer").setup({})
