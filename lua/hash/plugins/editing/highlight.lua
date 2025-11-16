078041f3d2b46e1900917a0066fac6f18d07ca23
Hashino <iam.ravara@gmail.com>
2025-11-16 15:03:51 -0300

    async treesitter install
    better setup ux

--- lua/hash/plugins/editing/highlight.lua
+++ lua/hash/plugins/editing/highlight.lua
@@ -6,7 +6,7 @@ vim.pack.add({
 require("nvim-treesitter.install").prefer_git = true
 
 require("nvim-treesitter.configs").setup({
-  sync_install = true,
+  sync_install = false,
 
   modules = {},
   ignore_install = {},
@@ -30,10 +30,10 @@ require("nvim-treesitter.configs").setup({
     additional_vim_regex_highlighting = false,
   },
 
-  auto_install = true,       -- Autoinstall languages that are not installed yet
+  auto_install = true, -- Autoinstall languages that are not installed yet
 
   disable = function(_, buf)
-    local max_filesize = 100 * 1024       -- 100 KB
+    local max_filesize = 100 * 1024 -- 100 KB
     local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
     if ok and stats and stats.size > max_filesize then
       return true
