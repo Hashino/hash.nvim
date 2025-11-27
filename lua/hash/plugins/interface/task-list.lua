73b3ff1752e426e83182101517f6deaeca0b0af6
Hashino <iam.ravara@gmail.com>
2025-11-27 12:36:00 -0300

    update

--- lua/hash/plugins/interface/task-list.lua
+++ lua/hash/plugins/interface/task-list.lua
@@ -7,21 +7,14 @@ require("doing").setup({
   show_remaining = false,
   show_messages = false,
 
-  ignored_buffers = { "trouble", "no-neck-pain", ".tasks", },
+  ignored_buffers = { "trouble", ".tasks", },
 
   winbar = { enabled = false, },
 })
 
 local doing = require("doing")
 
-vim.keymap.set("n", "<leader>da", doing.add,
-  { desc = "[D]oing: [A]dd", })
-
-vim.keymap.set("n", "<leader>de", doing.edit,
-  { desc = "[D]oing: [E]dit", })
-
-vim.keymap.set("n", "<leader>dn", doing.done,
-  { desc = "[D]oing: Do[n]e", })
-
-vim.keymap.set("n", "<leader>dt", doing.toggle,
-  { desc = "[D]oing: [T]oggle", })
+vim.keymap.set("n", "<leader>da", doing.add, { desc = "[D]oing: [A]dd", })
+vim.keymap.set("n", "<leader>de", doing.edit, { desc = "[D]oing: [E]dit", })
+vim.keymap.set("n", "<leader>dn", doing.done, { desc = "[D]oing: Do[n]e", })
+vim.keymap.set("n", "<leader>dt", doing.toggle, { desc = "[D]oing: [T]oggle", })
